2921003466626d21f50c84b293f6c583b102973b
jinweelee <jinweelee@gmail.com>
2020-06-12 16:13:17 +0800

    initial commit

---
+++ R_dependencies.R
@@ -0,0 +1,27 @@
+# xml2
+# ggplot2
+# cowplot
+# knitr
+# rmarkdown
+# plotly
+# shiny {curl: libcurl4-opnessl-dev, openssl: libssl-dev}
+# tidyverse {curl: libcurl4-opnessl-dev, openssl: libssl-dev}
+# wesanderson
+# learnr
+# data.table
+
+
+
+##rstudioapi::convertTheme('~/Desktop/jwthemenew.tmTheme', outputLocation = '~/Desktop/')
+# Extra notes
+# Turn on variable tracing
+# highlight functions
+# change bracket colors to white
+
+
+
+
+# Additional software installation
+# Rtools download and setup -> add to Renviron path
+
+ 
